1aeca42beeb499b074f690a90ed81151e1bc22b1
Mustafa Bursali <m.bursali16@gmail.com>
2023-04-26 13:43:10 +0300

    enabled additional cards to deck

--- game-service/src/game/decks.ts
+++ game-service/src/game/decks.ts
@@ -9,8 +9,9 @@ class Deck {
         this.points = [acePoint, 2, 3, 4, 5, 6, 7, 8, 9, 10, jackPoint, queenPoint, kingPoint]
     }
 
-    createDeck(howManyDecks: number) {
+    createDeck(howManyDecks: number, additionalCards?: object[]) {
         let deck:object[] = []
+        
         for(let d = 0; d < howManyDecks; d++) {
             // to create a single deck
             for(let i = 0; i < this.suits.length; i++) {
@@ -20,6 +21,7 @@ class Deck {
             }
         }
 
+        if(additionalCards) return [...deck, ...additionalCards]
         return deck;
     }
 
---
+++ game-service/src/server.ts
@@ -0,0 +1,6 @@
+import Deck from "./game/decks";
+
+const deck = new Deck(1,1,1,1)
+const additionalCards = []
+
+console.log(deck.createDeck(1))
